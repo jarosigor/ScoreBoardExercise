68d07c54fb585c85b8bca097271655011b7a2490
jarosigor <55409506+jarosigor@users.noreply.github.com>
2025-03-12 00:02:09 +0100

    Remove uuids as it s not necessary for the requirements.

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.Objects;
-import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +8,12 @@ import lombok.Setter;
 @Setter
 public class Team {
 
-    private final UUID uuid;
     private final String name;
     private Integer wins;
     private Integer draws;
     private Integer losses;
 
     public Team(String name) {
-        this.uuid = UUID.randomUUID();
         this.name = name;
         this.wins = 0;
         this.draws = 0;
@@ -32,6 +29,6 @@ public class Team {
 
     @Override
     public int hashCode() {
-        return Objects.hash(getUuid(), getName());
+        return Objects.hash(getName());
     }
 }
