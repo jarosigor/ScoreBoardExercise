d9cd53df901f2c507b1eab15d1ca282bb3c9ce13
jarosigor <55409506+jarosigor@users.noreply.github.com>
2025-03-12 00:11:39 +0100

    Add new validation checks with some new cases.

--- src/main/java/service/ScoreBoardValidationService.java
+++ src/main/java/service/ScoreBoardValidationService.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.Objects;
 import lombok.experimental.UtilityClass;
 import model.Match;
+import model.Scoreboard;
+import model.Team;
 
 @UtilityClass
 public class ScoreBoardValidationService {
@@ -19,18 +21,38 @@ public class ScoreBoardValidationService {
         if (match.getEndTime() != null) {
             throw ErrorMessages.reportFinishedMatch();
         }
+        if (Boolean.FALSE.equals(match.getInProgress())) {
+            throw ErrorMessages.reportMatchNotInProgress();
+        }
     }
 
     void checkMatchIsValid(Match match) {
         if (match == null) {
             throw ErrorMessages.reportNullArgument();
         }
+        if (areValuesNonNull(match.getHomeTeam().getName(), match.getAwayTeam().getName())) {
+            throw ErrorMessages.reportNullArgument();
+        }
         if (match.getEndTime() != null) {
-            throw new IllegalStateException("Match is already finished");
+            throw ErrorMessages.reportFinishedMatch();
         }
         if (areTeamsEqual(match)) {
             throw ErrorMessages.reportNonValidTeamName();
         }
+    }
+
+    void checkMatchIsInScoreboard(Match match, Scoreboard scoreboard) {
+        if (isMatchNotInTheScoreboard(match, scoreboard)) {
+            throw ErrorMessages.reportMatchNotInScoreboard();
+        }
+    }
+
+    boolean isMatchNotInTheScoreboard(Match match, Scoreboard scoreboard) {
+        return !scoreboard.getMatches().contains(match);
+    }
+
+    boolean isTeamAlreadyCreated(String teamName, Scoreboard scoreboard) {
+        return scoreboard.getTeams().contains(new Team(teamName));
     }
 
     private boolean areValuesNonNull(Object... values) {
